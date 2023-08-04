dfea9ca4296256633b1a199e841c3cdc421f7428
digitalyoshixi <davidchenyuhe@proton.me>
2023-08-04 18:48:25 -0400

    better copying and pasteing

--- config.h
+++ config.h
@@ -229,6 +229,7 @@ ResourcePref resources[] = {
  */
 static MouseShortcut mshortcuts[] = {
 	/* mask                 button   function        argument       release */
+	{ XK_ANY_MOD,           Button3, selpaste,       {.i = 0},      1 },
 	{ XK_NO_MOD,            Button4, kscrollup,      {.i = 1} },
 	{ XK_NO_MOD,            Button5, kscrolldown,    {.i = 1} },
 	{ XK_ANY_MOD,           Button2, selpaste,       {.i = 0},      1 },
