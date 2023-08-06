6efbba103bc8902b352c9321a26f49d260445cbd
digitalyoshixi <davidchenyuhe@proton.me>
2023-08-06 17:17:52 -0400

    change palette again

--- config.h
+++ config.h
@@ -115,31 +115,31 @@ float alphaUnfocus;
 static const char *colorname[] = {
 
 	/* 8 normal colors */
-	[0] = "#494d4a", /* grey */
-	[1] = "#60da81", /* sea green */
-	[2] = "#6b786d", /* murky stone   */
-	[3] = "#3c4f40", /* muck  */
-	[4] = "#546e5a", /* rot    */
-	[5] = "#689f74", /* jade */
-	[6] = "#aeb4af", /* stone */
-	[7] = "#a4e3b0", /* light sea green  */
+	[0] = "#5c645e", /* black   */
+	[1] = "#60da81", /* red     */
+	[2] = "#6b786d", /* green   */
+	[3] = "#3c4f40", /* yellow  */
+	[4] = "#546e5a", /* blue    */
+	[5] = "#689f74", /* magenta */
+	[6] = "#aeb4af", /* cyan    */
+	[7] = "#a4e3b0", /* white   */
 
 	/* 8 bright colors */
-	[8]  = "#afb3be", /* soulless */
-	[9]  = "#dc8f44", /* dull orange     */
-	[10] = "#6babad", /* medical blue   */
-	[11] = "#acacac", /* flat stone  */
-	[12] = "#c3f904", /* yellow green    */
-	[13] = "#1efa00", /* neon green */
-	[14] = "#8bf202", /* lime    */
-  	[15] = "#719aef", /* sea blue   */
+	[8]  = "#afb3be", /* black   */
+	[9]  = "#dc8f44", /* red     */
+	[10] = "#6babad", /* green   */
+	[11] = "#acacac", /* yellow  */
+	[12] = "#c3f904", /* blue    */
+	[13] = "#1efa00", /* magenta */
+	[14] = "#00cc99", /* cyan    */
+	[15] = "#719aef", /* white   */
 
 	[255] = 0,
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#add8e6", /* 256 -> cursor */
 	"#555555", /* 257 -> rev cursor*/
-	"#282d29", /* 258 -> bg */
-	"#bffbca", /* 259 -> fg */
+	"#000000", /* 258 -> bg */
+	"#ffffff", /* 259 -> fg */
 };
 
 
